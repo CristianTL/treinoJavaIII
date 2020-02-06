6671614bf9429a5c7bf0636e21f6fbb2c06716e3
CristianTL <thomazcl1@hotmail.com>
2020-02-06 17:04:05 -0300

    Aplicação do resultset e resultSetMetaData para o jTable

---
+++ src/br/com/treino/ModeloTabela.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.treino;
+
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import javax.swing.table.AbstractTableModel;
+
+/**
+ *
+ * @author User
+ */
+public class ModeloTabela extends AbstractTableModel {
+    
+    private int colunas, linhas;
+    private ResultSet resultset; 
+    private ResultSetMetaData rsMetaData;
+    
+    public ModeloTabela(ResultSet resultset) throws SQLException{
+        rsMetaData = resultset.getMetaData();
+        this.resultset = resultset;
+        resultset.last(); //ultimo registro
+        linhas = resultset.getRow(); // pega o numero da linha em que se encontra
+    }
+
+    @Override
+    public int getRowCount() {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return linhas;
+    }
+
+    @Override
+    public int getColumnCount() {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        colunas  = 0;
+        try{
+            colunas = rsMetaData.getColumnCount();
+        } catch (SQLException erro){
+            System.out.println("Erro ao ler qtde colunas" + erro);
+        }
+        return colunas;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
