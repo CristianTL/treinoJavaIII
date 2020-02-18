a41edb4a5e9bdf682768c4479e9d1d95a863eaf7
CristianTL <thomazcl1@hotmail.com>
2020-02-18 15:57:30 -0300

    finalização do resultSetPessoa no ModeloTabelaPessoa

--- src/br/com/cadastromodelotabela/ModeloTabelaPessoa.java
+++ src/br/com/cadastromodelotabela/ModeloTabelaPessoa.java
@@ -5,7 +5,9 @@
  */
 package br.com.cadastromodelotabela;
 
+import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import javax.swing.table.AbstractTableModel;
 
@@ -22,6 +24,25 @@ public class ModeloTabelaPessoa extends AbstractTableModel{
         "Codigo", "Nome da Pessoa", "Telefone", "Email"
     };
     
+    public void setResult(ResultSet resultset) throws SQLException{
+        resultSetPessoas = new ArrayList<String[]>();
+        while(resultset.next()){
+            String[] linha = {
+                resultset.getString("codigo"), 
+                resultset.getString("nome"), 
+                resultset.getString("fone"), 
+                resultset.getString("email")
+            };
+            resultSetPessoas.add(linha);
+        }
+        fireTableStructureChanged();
+    }
+    
+    public void deletarLinha(int linha){
+        resultSetPessoas.remove(linha);
+        fireTableRowsDeleted(linha, linha);
+    }
+    
     @Override
     public int getRowCount() {
         //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
@@ -36,7 +57,8 @@ public class ModeloTabelaPessoa extends AbstractTableModel{
 
     @Override
     public Object getValueAt(int rowIndex, int columnIndex) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String[] linha = resultSetPessoas.get(rowIndex);
+        return linha[columnIndex];
     }
     
 }
