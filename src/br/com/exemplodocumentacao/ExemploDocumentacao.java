d2e080a6d3e74930490a23beed47440eaa21f4f6
CristianTL <thomazcl1@hotmail.com>
2020-10-30 13:46:50 -0300

    Continuação de boas praticas e executar javadoc

---
+++ src/br/com/exemplodocumentacao/ExemploDocumentacao.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.exemplodocumentacao;
+
+/**
+ *
+ * @author Thomaz Sobrenome
+ * Programador
+ */
+public class ExemploDocumentacao {
+    private double valor; // server para não sei o que
+    
+    /**
+     *
+     * Metodo usado para imprimir valor
+     * @param args veio de não sei o que 
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+    /**
+     *
+     * Metodo usado para imprimir valor
+     * @param valor veio de não sei o que 
+     */
+    public void imprimeValor(double valor){
+        
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+}
