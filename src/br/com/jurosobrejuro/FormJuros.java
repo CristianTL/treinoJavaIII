faaee0b7b4cf459a46e65fb1349070d1c83298f2
CristianTL <thomazcl1@hotmail.com>
2020-11-23 12:10:42 -0300

    Juros compostos usando Math

--- src/br/com/jurosobrejuro/FormJuros.java
+++ src/br/com/jurosobrejuro/FormJuros.java
@@ -38,6 +38,7 @@ public class FormJuros extends javax.swing.JFrame {
         tfDiasAtraso = new javax.swing.JTextField();
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -78,6 +79,13 @@ public class FormJuros extends javax.swing.JFrame {
             }
         });
 
+        jButton3.setText("Juros Compostos com Math");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -103,8 +111,10 @@ public class FormJuros extends javax.swing.JFrame {
                             .addGroup(layout.createSequentialGroup()
                                 .addComponent(jButton1)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(jButton2)))
-                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
+                                .addComponent(jButton2)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(jButton3)))
+                        .addContainerGap(106, Short.MAX_VALUE))))
             .addGroup(layout.createSequentialGroup()
                 .addGap(43, 43, 43)
                 .addComponent(jLabel5)
@@ -132,7 +142,8 @@ public class FormJuros extends javax.swing.JFrame {
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jButton1)
-                    .addComponent(jButton2))
+                    .addComponent(jButton2)
+                    .addComponent(jButton3))
                 .addGap(24, 24, 24)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel5)
@@ -156,6 +167,11 @@ public class FormJuros extends javax.swing.JFrame {
         // TODO add your handling code here:
         calculo2();
     }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        // TODO add your handling code here:
+        calculo3();
+    }//GEN-LAST:event_jButton3ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -195,6 +211,7 @@ public class FormJuros extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
@@ -230,6 +247,20 @@ public class FormJuros extends javax.swing.JFrame {
         for(int x = 1; x <= diasAtraso; x++ ){
             valorFinalPagar += valorFinalPagar * taxaJuros / 100;
         }      
+        
+        labelValorFinal.setText(""+valorFinalPagar);
+    }
+    
+    /**
+     * Usando biblioteca Math
+     * Valor Final = ValorParcela * (1 + TaxaJuros) ^ diasAtraso
+     */
+    private void calculo3() {
+        double valorParcela, taxaJuros, diasAtraso, valorFinalPagar;
+        valorParcela = Double.parseDouble(tfValorParcela.getText());
+        taxaJuros = Double.parseDouble(tfTaxaJuros.getText());
+        diasAtraso = Integer.parseInt(tfDiasAtraso.getText());
+        valorFinalPagar = valorParcela * Math.pow((1 + taxaJuros/100), diasAtraso);  
         
         labelValorFinal.setText(""+valorFinalPagar);
     }
